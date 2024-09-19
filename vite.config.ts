0cd4deaa39acbd949b959acb2eac55ba69009ee3
mahatohariom <mahatohariom31@gmail.com>
2024-09-19 19:43:04 +0545

    setp home page

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mahatohariom.github.io",
+  // base: "https://mahatohariom.github.io",
+  base: "/vite-deploy-demo/",
 });
